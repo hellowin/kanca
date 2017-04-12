e21301f35c3f647522d9bab2d6d72d071684640b
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-12 09:40:03 +0700

    update footer

--- src/domain/layout/component/Footer.js
+++ src/domain/layout/component/Footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import config from 'config';
 
 class Footer extends Component {
   render() {
+    const homepage = config.homepage;
     return (
       <footer className="app-footer">
-        <a href="http://coreui.io">CoreUI</a> &copy; 2017 creativeLabs.
+        <a href={homepage}>Kanca</a> &copy; 2017 Hellowin.
         <span className="float-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
       </footer>
     )
