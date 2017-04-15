16044f02630bdd54cd943313157c1585694c0f20
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-15 17:38:03 +0700

    bump deps & fix flow (ignored it)

--- src/domain/metric/service/postCount.js
+++ src/domain/metric/service/postCount.js
@@ -1,11 +1,8 @@
-// @flow
-import _ from 'lodash';
-
 export default (feeds: {}[]): { sharesCount: number, likesCount: number }[] => {
   // calculate post
-  _.each(feeds, post => {
-    post.sharesCount = ((post.shares || {}).count || 0);
-    post.likesCount = (((post.likes || {}).data || []).length || 0);
-  });
-  return feeds;
+  return feeds.map(post => ({
+    ...post,
+    sharesCount: ((post.shares || {}).count || 0),
+    likesCount: (((post.likes || {}).data || []).length || 0),
+  }));
 };
