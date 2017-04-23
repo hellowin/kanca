a1d3e8a49b2bd0727bbbb4c6b7563d5bd312d748
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-24 05:55:57 +0700

    fix versioning

--- src/domain/layout/component/Footer.js
+++ src/domain/layout/component/Footer.js
@@ -3,12 +3,12 @@ import config from 'config';
 
 class Footer extends Component {
   render() {
-    const homepage = config.homepage;
+    const { homepage, version } = config;
     return (
       <footer className="app-footer">
         <a href={homepage}>Kanca</a> &copy; 2017 Hellowin.&nbsp;|&nbsp;
-        <a href="https://hellowin.github.io/kanca/privacypolicy.html" target="_blank">Privacy Policy</a>&nbsp;|&nbsp;
-        <span>v{config.commitVersion}</span>
+        <a href="https://hellowin.github.io/kanca/privacypolicy.html" target="_blank">Privacy Policy</a>
+        {version ? <span>&nbsp;|&nbsp;{version}</span> : ''}
         <span className="float-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
       </footer>
     )
