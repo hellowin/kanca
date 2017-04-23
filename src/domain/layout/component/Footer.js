71bebd730b2ca2cbd2c40dbc28fd5f1812aba61d
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-23 22:59:58 +0700

    add commit version

--- src/domain/layout/component/Footer.js
+++ src/domain/layout/component/Footer.js
@@ -6,7 +6,9 @@ class Footer extends Component {
     const homepage = config.homepage;
     return (
       <footer className="app-footer">
-        <a href={homepage}>Kanca</a> &copy; 2017 Hellowin. | <a href="https://hellowin.github.io/kanca/privacypolicy.html" target="_blank">Privacy Policy</a>
+        <a href={homepage}>Kanca</a> &copy; 2017 Hellowin.&nbsp;|&nbsp;
+        <a href="https://hellowin.github.io/kanca/privacypolicy.html" target="_blank">Privacy Policy</a>&nbsp;|&nbsp;
+        <span>v{config.commitVersion}</span>
         <span className="float-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
       </footer>
     )
