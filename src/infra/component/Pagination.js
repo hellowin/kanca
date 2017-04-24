40e8e452d7ca0794cecf30ebcdfce6139033b402
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-24 21:07:54 +0700

    add pagination jump

--- src/infra/component/Pagination.js
+++ src/infra/component/Pagination.js
@@ -57,6 +57,12 @@ class Pagination extends React.Component {
 
     // available pages
     const pages = [
+      (<li key="first" className={page <= 1 ? 'disabled page-item' : 'page-item'}>
+        <a className="page-link" href="#" aria-label="First" onClick={this.goToPage(1)}>
+          <span aria-hidden="true">««</span>
+          <span className="sr-only">First</span>
+        </a>
+      </li>),
       (<li key="prev" className={page <= 1 ? 'disabled page-item' : 'page-item'}>
         <a className="page-link" href="#" aria-label="Previous" onClick={this.goToPage(page - 1)}>
           <span aria-hidden="true">«</span>
@@ -69,7 +75,13 @@ class Pagination extends React.Component {
           <span aria-hidden="true">»</span>
           <span className="sr-only">Next</span>
         </a>
-      </li>)
+      </li>),
+      (<li key="last" className={page >= totalPage ? 'disabled page-item' : 'page-item'}>
+        <a className="page-link" href="#" aria-label="Last" onClick={this.goToPage(totalPage)}>
+          <span aria-hidden="true">»»</span>
+          <span className="sr-only">Last</span>
+        </a>
+      </li>),
     ];
 
     const pagination = (
