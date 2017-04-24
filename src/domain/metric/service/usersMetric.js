e2d31afcbf867289c1fa2873af2e9a6594f3fb6f
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-24 13:45:06 +0700

    fix time series chart

--- src/domain/metric/service/usersMetric.js
+++ src/domain/metric/service/usersMetric.js
@@ -4,9 +4,12 @@ import userMetricer from './userMetric';
 import type { UserMetric } from './userMetric';
 
 export type UsersMetric = {
+  totalMembers: number,
   userMetrics: UserMetric[],
   uniqueUsersPosts(): UserMetric[],
   uniqueUsersComments(): UserMetric[],
+  uniqueUsersPostsLikes(): UserMetric[],
+  uniqueUsersPostsShares(): UserMetric[],
   sortByPostsCount(): UserMetric[],
   sortByCommentsCount(): UserMetric[],
   sortByPostsLikesCount(): UserMetric[],
@@ -14,12 +17,16 @@ export type UsersMetric = {
 }
 
 export default (members: Member[], posts: Post[], comments: Comment[]): UsersMetric => {
+  const totalMembers = members.length;
   const userMetrics: UserMetric[] = userMetricer(members, posts, comments);
 
   return {
+    totalMembers,
     userMetrics,
     uniqueUsersPosts: () => userMetrics.filter(met => met.postsCount !== 0),
     uniqueUsersComments: () => userMetrics.filter(met => met.commentsCount !== 0),
+    uniqueUsersPostsLikes: () => userMetrics.filter(met => met.postsLikesCount !== 0),
+    uniqueUsersPostsShares: () => userMetrics.filter(met => met.postsSharesCount !== 0),
     sortByPostsCount: () => _.sortBy(userMetrics, 'postsCount').reverse(),
     sortByCommentsCount: () => _.sortBy(userMetrics, 'commentsCount').reverse(),
     sortByPostsLikesCount: () => _.sortBy(userMetrics, 'postsLikesCount').reverse(),
