14603ad2aa25f3df043e30fd7718cdd04627bbb0
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-29 06:15:23 +0700

    add members page

--- src/domain/metric/container/Members.js
+++ src/domain/metric/container/Members.js
@@ -3,8 +3,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Member from '../component/Member';
 import Loading from 'infra/component/Loading';
+import Card from 'infra/component/Card';
 import Pagination from 'infra/component/Pagination';
 import usersMetricer from '../service/usersMetric';
+
 
 const mapStateToProps = state => ({
   loading: state.group.loading,
@@ -20,7 +22,13 @@ const GroupMembers = props => {
 
   return !loading ? (
     <div>
-      <h1 className="h3">Group Members</h1>
+      <h1 className="h3 mb-1">Members Summary</h1>
+      <Card>
+        <div>Total members: {metric.totalMembers()}</div>
+        <div>Total members posts: {metric.uniqueUsersPosts().length}</div>
+        <div>Total members comments: {metric.uniqueUsersComments().length}</div>
+      </Card>
+      <h1 className="h3 mb-1">Group Members</h1>
       <Pagination list={metric.userMetrics} ChildNode={Member} />
     </div>
   ) : <Loading />;
