8a963e8bb8f4bcc2eb88c6a6968566be3a168675
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-30 11:30:31 +0700

    fix bug on group header selection (#86)

--- src/domain/layout/component/Header.js
+++ src/domain/layout/component/Header.js
@@ -2,58 +2,31 @@
 import React, { Component } from 'react';
 import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
 import { connect } from 'react-redux';
-import moment from 'moment-timezone';
 import userRepo from 'infra/repo/user';
-import groupRepo from 'infra/repo/group';
 import loc from 'infra/service/location';
+import GroupStatus from './GroupStatus';
 
 const mapStateToProps = state => ({
   loading: state.user.loading,
   profile: state.user.profile,
-  group: state.group.selected,
-  updatedTime: state.group.updatedTime,
 });
 
 class Header extends Component {
 
   state: {
     dropdownOpen: boolean,
-    deltaTime: string,
-    diffTime: number,
   }
 
-  getTime: Function
   toggle: Function
-  intervalTime: any
 
   constructor(props) {
     super(props);
 
-    this.getTime = this.getTime.bind(this);
     this.toggle = this.toggle.bind(this);
-
-    const { updatedTime } = props;
-    this.getTime(updatedTime);
 
     this.state = {
       dropdownOpen: false,
-      deltaTime: moment(new Date(updatedTime)).toNow(),
-      diffTime: moment(new Date(updatedTime)).diff(moment()),
     };
-  }
-
-  getTime(updatedTime) {
-    clearInterval(this.intervalTime);
-    this.intervalTime = setInterval(() => {
-      const deltaTime = moment(new Date(updatedTime)).toNow();
-      const diffTime = moment(new Date(updatedTime)).diff(moment());
-      this.setState({ deltaTime, diffTime });
-    }, 5000);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { updatedTime } = nextProps;
-    this.getTime(updatedTime);
   }
 
   toggle() {
@@ -78,9 +51,7 @@ class Header extends Component {
   }
 
   render() {
-    const { profile, loading, group } = this.props;
-    const { deltaTime, diffTime } = this.state;
-    const refreshTime = diffTime + 15 * 60000 < 0; // 15 minutes
+    const { profile, loading } = this.props;
 
     return (
       <header className="app-header navbar" style={{ paddingRight: '15px' }}>
@@ -90,9 +61,9 @@ class Header extends Component {
           <li className="nav-item">
             <a className="nav-link navbar-toggler sidebar-toggler" onClick={this.sidebarToggle} href="#">&#9776;</a>
           </li>
-          {group ? (<li className="nav-item">
-            <span>Selected Group: <b>{group.name}</b> | Updated {deltaTime} ago {refreshTime ? <span> | <button className="btn btn-primary btn-sm" style={{ cursor: 'pointer' }} onClick={() => groupRepo.refreshGroup()}>Refresh</button></span> : ''} </span>
-          </li>) : ''}
+          <li className="nav-item">
+            <GroupStatus />
+          </li>
         </ul>
         {!loading ? (<ul className="nav navbar-nav ml-auto">
           <li className="nav-item">
