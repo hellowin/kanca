e185c4be59b9442dc2b4898b513a5f0d96f0bc61
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-30 08:18:48 +0700

    fix word count async

--- src/domain/metric/component/WordCloud.js
+++ src/domain/metric/component/WordCloud.js
@@ -2,24 +2,27 @@
 import React from 'react';
 import _ from 'lodash';
 import Measure from 'react-measure';
-import Cloud from 'react-d3-cloud';
+import Cloud from './Cloud';
 import Card from 'infra/component/Card';
 
 import type { TimeRangeMetric } from '../service/timeRangeMetric'
 
-const calculate = (type: string, metric: TimeRangeMetric): { word: string, count: number }[] => {
-  const postsWordCount = metric.postsMetric.wordCount();
-  const commsWordCount = metric.commentsMetric.wordCount();
+const calculate = (type: string, metric: TimeRangeMetric): Promise<{ word: string, count: number }[]> => {
+  const promPostsWordCount = metric.postsMetric.wordCount();
+  const promCommsWordCount = metric.commentsMetric.wordCount();
 
   switch (type) {
     case 'all':
     default:
       const allCount: { [string]: { word: string, count: number } } = {};
-      [...postsWordCount, ...commsWordCount].forEach(wor => {
-        if (!allCount[wor.word]) allCount[wor.word] = { word: wor.word, count: 0 };
-        allCount[wor.word].count += wor.count;
-      });
-      return _.values(allCount);
+      return Promise.all([promPostsWordCount, promCommsWordCount])
+        .then(([postsWordCount, commsWordCount]) => {
+          [...postsWordCount, ...commsWordCount].forEach(wor => {
+            if (!allCount[wor.word]) allCount[wor.word] = { word: wor.word, count: 0 };
+            allCount[wor.word].count += wor.count;
+          });
+          return _.values(allCount);
+        });
   }
 }
 
@@ -31,31 +34,64 @@ class WordCloud extends React.Component {
     type: 'all',
   }
 
-  render() {
-    const { metric, type, title } = this.props;
+  state : {
+    loading: boolean,
+    data: { text: string, value: number }[],
+  }
+
+  generateData: Function
+
+  constructor(props: Object) {
+    super(props);
+
+    this.generateData = this.generateData.bind(this);
+
+    this.state = {
+      loading: true,
+      data: [],
+    };
+  }
+
+  componentWillMount() {
+    this.generateData(this.props);
+  }
+
+  componentWillReceiveProps(nextProps: Object) {
+    this.generateData(nextProps);
+  }
+
+  generateData(props: Object) {
+    this.setState({ loading: true, data: [] });
+    const { metric, type } = this.props;
 
     const fixType = type || 'all';
 
-    const rawData = calculate(fixType, metric).map(wor => ({ text: wor.word, value: wor.count }));
-    const data = _.sortBy(rawData, 'value').reverse().slice(0, 500);
-    
-    const fontSizeMapper = word => Math.log2(word.value) * 5;
-    const rotate = word => word.value % 360;
+    calculate(fixType, metric)
+      .then(res => {
+        const rawData = res.map(wor => ({ text: wor.word, value: wor.count }));
+        const data = _.sortBy(rawData, 'value').reverse().slice(0, 500);
+        return data;
+      })
+      .then(data => this.setState({ loading: false, data }))
+      .catch(() => this.setState({ loading: false }));
+  }
 
+  render() {
+    const { title } = this.props;
+    const { loading, data } = this.state;
+    
     return (
-      <Measure>
-        { dimensions => (
-          <Card>
-            <h3>{title}</h3>
+      <Card>
+        <h3>{title}</h3>
+        {!loading ? (<Measure>
+          { dimensions => (
             <Cloud
               width={dimensions.width - 30}
               data={data}
-              fontSizeMapper={fontSizeMapper}
-              rotate={rotate}
             />
-          </Card>
-        )}
-      </Measure>
+          )}
+        </Measure>) : <div>Counting words...</div>}
+      </Card>
     );
   };
 
