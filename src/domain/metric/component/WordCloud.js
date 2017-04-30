48c7125d1851bc1f2e72c1b69a4bde88723f4d8d
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-04-30 16:49:29 +0700

    improve word cloud

--- src/domain/metric/component/WordCloud.js
+++ src/domain/metric/component/WordCloud.js
@@ -70,7 +70,7 @@ class WordCloud extends React.Component {
     calculate(fixType, metric)
       .then(res => {
         const rawData = res.map(wor => ({ text: wor.word, value: wor.count }));
-        const data = _.sortBy(rawData, 'value').reverse().slice(0, 500);
+        const data = _.sortBy(rawData, 'value').reverse().filter(dat => dat.value > 1).slice(0, 500);
         return data;
       })
       .then(data => this.setState({ loading: false, data }))
@@ -97,6 +97,8 @@ class WordCloud extends React.Component {
     } else {
       content = <div>No words found.</div>
     }
+
+    console.log(data)
 
     return (
       <Card>
