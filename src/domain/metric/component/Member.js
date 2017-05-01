2b19c8e80dcbe910f55e61d73988824b5a66b215
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-05-01 22:58:17 +0700

    fix members

--- src/domain/metric/component/Member.js
+++ src/domain/metric/component/Member.js
@@ -14,18 +14,26 @@ const Post = (props: UserMetric) => {
     postsSharesCount,
   } = props;
 
+  const maxName = 40;
+
   return (
     <div style={{ width: '20rem', float: 'left', margin: '0px 10px 15px 0px', height: '10rem' }}>
       <Card>
         <div className="row">
-          {picture ? <div className="col-md-4"><img src={picture} className="rounded float-left" alt="Prof pic" style={{ width: '100%' }} /></div> : ''}
+
+          <div className="col-12 mb-1">
+            <a href={url} target="_blank"><p className="card-text">{(name || '').length > maxName ? name.slice(0, maxName) + '...' : name}</p></a>
+          </div>
+
+          {picture ? <div className="col-md-4"><img src={picture} className="rounded" alt="Prof pic" style={{ width: '100%' }} /></div> : ''}
+
           <div className={picture ? 'col-md-6' : 'col-md-12'}>
-            <a href={url} target="_blank"><p className="card-text">{name}</p></a>
             <div>Total posts: {postsCount}</div>
             <div>Posts likes: {postsLikesCount}</div>
             <div>Posts shares: {postsSharesCount}</div>
             <div>Total comments: {commentsCount}</div>
           </div>
+
         </div>
       </Card>
     </div>
