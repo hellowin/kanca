b6c49a7aa08c0454c99fcc05f7e318f547571d7c
Reyhan Sofian <computecoholic@gmail.com>
2017-05-07 14:19:28 +0700

    fix login responsive

--- src/domain/user/container/Login.js
+++ src/domain/user/container/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import MediaQuery from 'react-responsive';
 import userRepo from 'infra/repo/user';
 import loc from 'infra/service/location';
 
@@ -25,19 +26,25 @@ class Login extends React.Component {
     return (
       <div className="container" style={{ marginTop: '100px' }}>
         <div className="row justify-content-center">
-          <div className="col-8">
-            <div className="card-group mb-0">
-              <div className="card p-2">
-                <div className="card-block text-center">
-                  <div>
-                    <h2>Login</h2>
-                    <p>Welcome to Kanca. Open source Facebook Group Analytics.</p>
-                    <button type="button" className="btn btn-primary active mt-1" onClick={() => userRepo.login()} style={{ cursor: 'pointer' }}>Login by Facebook</button>
+          <MediaQuery minDeviceWidth={320} maxWidthDevice={736} minWidth={320} maxWidth={736}>
+            {(matches) => {
+              return (
+                <div className={`col-${matches ? '12' : '8'}`}>
+                  <div className="card-group mb-0">
+                    <div className="card p-2">
+                      <div className="card-block text-center">
+                        <div>
+                          <h2>Login</h2>
+                          <p>Welcome to Kanca. Open source Facebook Group Analytics.</p>
+                          <button type="button" className="btn btn-primary active mt-1" onClick={() => userRepo.login()} style={{ cursor: 'pointer' }}>Login by Facebook</button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
+              )
+            }}
+          </MediaQuery>
         </div>
       </div>
     );
