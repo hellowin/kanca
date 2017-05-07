933fa348155af4e9d3f97a884e9b250684e1e21e
Reyhan Sofian <computecoholic@gmail.com>
2017-05-07 09:17:34 +0700

    Fix mobile view (#123)
    * add max width on main container
    
    * add react responsive
    
    * adjust group input
    
    * adjust group info card
    
    * fix input group (again)
    
    * adjust group feed mobile view
    
    * remove important
    
    * add flow type

--- src/domain/group/component/GroupInfo.js
+++ src/domain/group/component/GroupInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import groupRepo from 'infra/repo/group';
 import loc from 'infra/service/location';
 
@@ -11,24 +12,33 @@ const selectGroup = groupId => e => {
 const Card = props => {
   const { id, name, cover, owner, privacy } = props;
   return (
-    <div className="card mr-1" style={{ width: '20rem', float: 'left' }}>
-      <div className="card-img-top" style={{
-        height: '100px',
-        backgroundImage: `url("${cover}")`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: '50% 50%',
-      }}></div>
-      <div className="card-block">
-        <p><b>{name}</b></p>
-        <span className="mr-1"><i className="fa fa-lock"></i> {privacy}</span>
-        {owner ? <span className="mr-1"><i className="fa fa-user"></i> {owner.name}</span> : ''}
-      </div>
-      <div className="card-block pt-0" style={{ textAlign: 'right' }}>
-        <a href={`https://www.facebook.com/groups/${id}/`} target="_blank" className="btn btn-primary btn-sm">Open in FB</a>
-        &nbsp;
-        <a href="#" onClick={selectGroup(id)} className="btn btn-primary btn-sm">Select</a>
-      </div>
+    <div>
+      <MediaQuery minDeviceWidth={320} maxWidthDevice={736} minWidth={320} maxWidth={736}>
+        {(matches) => {
+          const cardStyleOnMobile = matches ? { width: '100%', float: 'left' } : { width: '20rem', float: 'left' };
+          return (
+            <div className={`card ${matches ? '' : 'mr-1'}`} style={cardStyleOnMobile}>
+              <div className="card-img-top" style={{
+                height: '100px',
+                backgroundImage: `url("${cover}")`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: '50% 50%',
+              }}></div>
+              <div className="card-block">
+                <p><b>{name}</b></p>
+                <span className="mr-1"><i className="fa fa-lock"></i> {privacy}</span>
+                {owner ? <span className="mr-1"><i className="fa fa-user"></i> {owner.name}</span> : ''}
+              </div>
+              <div className="card-block pt-0" style={{ textAlign: 'right' }}>
+                <a href={`https://www.facebook.com/groups/${id}/`} target="_blank" className="btn btn-primary btn-sm">Open in FB</a>
+                &nbsp;
+                <a href="#" onClick={selectGroup(id)} className="btn btn-primary btn-sm">Select</a>
+              </div>
+            </div>
+          );
+        }}
+      </MediaQuery>
     </div>
   );
 }
