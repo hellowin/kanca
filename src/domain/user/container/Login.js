c037455372a877100248166c203a286a470f24cb
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-05-10 06:32:15 +0700

    remove exposed NODE_PATH on .env and use imported image instead (#144)
    * remove exposed NODE_PATH on .env and use imported image instead
    
    * expose NODE_PATH on build and test

--- src/domain/user/container/Login.js
+++ src/domain/user/container/Login.js
@@ -4,7 +4,9 @@ import MediaQuery from 'react-responsive';
 import userRepo from 'infra/repo/user';
 import loc from 'infra/service/location';
 import styles from './Login.style';
-import config from 'config';
+import githubLogo from 'img/github-logo.png';
+import kancaLogo from 'img/logo.png';
+import facebookLoginImg from 'img/facebook-login-button.png';
 
 const mapStateToProps = state => ({
   loggedIn: state.user.loggedIn,
@@ -25,8 +27,6 @@ class Login extends React.Component {
   }
 
   render() {
-    const prefix = config.env !== 'development' ? config.urlPrefix : '';
-
     return (
       <div style={styles.container}>
           <MediaQuery minDeviceWidth={320} maxWidthDevice={736} minWidth={320} maxWidth={736}>
@@ -36,14 +36,14 @@ class Login extends React.Component {
                   <div style={styles.header}>
                     <a href="https://hellowin.github.io/kanca" style={styles.siteName}>Kanca</a>
                     <a href="https://github.com/hellowin/kanca">
-                      <img src={`${prefix}/img/github-logo.png`} alt="github-logo" style={styles.logoGithub} />
+                      <img src={githubLogo} alt="github-logo" style={styles.logoGithub} />
                     </a>
                   </div>
                   <div style={styles.content}>
-                    <img src={`${prefix}/img/logo.png`} alt="logo"/>
+                    <img src={kancaLogo} alt="logo"/>
                     <span style={styles.description}>An app to analyze Facebook Group activity</span>
                     <img
-                      src={`${prefix}/img/facebook-login-button.png`}
+                      src={facebookLoginImg}
                       alt="facebook-login-button"
                       style={styles.loginBtn}
                       onClick={() => userRepo.login()}
