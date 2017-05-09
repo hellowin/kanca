32c395cec2ba5487bd552ac1161237ac380fafd4
Reyhan Sofian <computecoholic@gmail.com>
2017-05-09 15:02:11 +0700

    add active menu on sidebar (#135)
    * add active menu sidebar
    
    * rename method

--- src/domain/layout/component/Sidebar.js
+++ src/domain/layout/component/Sidebar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import NavLink from './NavLink';
+import uuid from 'uuid';
+
 import loc from 'infra/service/location';
 
 const mapStateToProps = state => ({
   location: state.routing.locationBeforeTransitions,
   feeds: state.group.feeds,
 });
-
-const goTo = path => e => {
-  e.preventDefault();
-  loc.push(path);
-}
 
 class Sidebar extends Component {
 
@@ -36,10 +34,10 @@ class Sidebar extends Component {
               <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-people"></i> Group</a>
               <ul className="nav-dropdown-items">
                 <li className="nav-item">
-                  <a href="" className="nav-link" onClick={goTo('/group/selection')}>Selection</a>
+                  <NavLink className="nav-link" to={loc.getUrl('/group/selection')}>Selection</NavLink>
                 </li>
                 {feeds.length > 0 ? <li className="nav-item">
-                  <a href="" className="nav-link" onClick={goTo('/group/feed')}>Feed</a>
+                  <NavLink className="nav-link" to={loc.getUrl('/group/feed')}>Feed</NavLink>
                 </li> : ''}
               </ul>
             </li>
@@ -48,17 +46,19 @@ class Sidebar extends Component {
               <a className="nav-link nav-dropdown-toggle" href="#" onClick={this.handleClick.bind(this)}><i className="icon-graph"></i> Metric</a>
               <ul className="nav-dropdown-items">
                 <li className="nav-item">
-                  <a href="" className="nav-link" onClick={goTo('/metric/summary')}>Summary</a>
+                  <NavLink className="nav-link" to={loc.getUrl('/metric/summary')}>Summary</NavLink>
                 </li>
-                {feeds.length > 0 ? <li className="nav-item">
-                  <a href="" className="nav-link" onClick={goTo('/metric/posts')}>Posts</a>
-                </li> : ''}
-                {feeds.length > 0 ? <li className="nav-item">
-                  <a href="" className="nav-link" onClick={goTo('/metric/comments')}>Comments</a>
-                </li> : ''}
-                {feeds.length > 0 ? <li className="nav-item">
-                  <a href="" className="nav-link" onClick={goTo('/metric/members')}>Members</a>
-                </li> : ''}
+                {feeds.length > 0 && [
+                  <li className="nav-item" key={uuid.v4()}>
+                    <NavLink className="nav-link" to={loc.getUrl('/metric/posts')}>Posts</NavLink>
+                  </li>,
+                  <li className="nav-item" key={uuid.v4()}>
+                    <NavLink className="nav-link" to={loc.getUrl('/metric/comments')}>FeedComments</NavLink>
+                  </li>,
+                  <li className="nav-item" key={uuid.v4()}>
+                  <NavLink className="nav-link" to={loc.getUrl('/metric/members')}>Members</NavLink>
+                  </li>
+                ]}
               </ul>
             </li> : ''}
 
--- src/infra/service/location.js
+++ src/infra/service/location.js
@@ -2,8 +2,10 @@
 import { browserHistory } from 'react-router';
 import config from 'config';
 
-export const push = (to: string) => browserHistory.push(config.urlPrefix + to);
+export const push = (to: string) => browserHistory.push(config.getUrl + to);
+export const getUrl = (to: string) => `${config.getUrl}${to}`;
 
 export default {
   push,
+  getUrl,
 };
