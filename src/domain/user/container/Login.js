cee429ef996029b0ff6e09f4bd507fa2745496ea
Lutfian Dwi Cahyono <lutfiandwica@gmail.com>
2017-05-09 23:49:42 +0700

    fix img src path (#142)
    * redesign Login page
    
    * fix img src path

--- src/domain/user/container/Login.js
+++ src/domain/user/container/Login.js
@@ -4,6 +4,7 @@ import MediaQuery from 'react-responsive';
 import userRepo from 'infra/repo/user';
 import loc from 'infra/service/location';
 import styles from './Login.style';
+import config from 'config';
 
 const mapStateToProps = state => ({
   loggedIn: state.user.loggedIn,
@@ -24,6 +25,8 @@ class Login extends React.Component {
   }
 
   render() {
+    const prefix = config.env !== 'development' ? config.urlPrefix : '';
+
     return (
       <div style={styles.container}>
           <MediaQuery minDeviceWidth={320} maxWidthDevice={736} minWidth={320} maxWidth={736}>
@@ -33,14 +36,14 @@ class Login extends React.Component {
                   <div style={styles.header}>
                     <a href="https://hellowin.github.io/kanca" style={styles.siteName}>Kanca</a>
                     <a href="https://github.com/hellowin/kanca">
-                      <img src="/img/github-logo.png" alt="github-logo" style={styles.logoGithub}/>
+                      <img src={`${prefix}/img/github-logo.png`} alt="github-logo" style={styles.logoGithub} />
                     </a>
                   </div>
                   <div style={styles.content}>
-                    <img src="/img/logo.png" alt="logo"/>
+                    <img src={`${prefix}/img/logo.png`} alt="logo"/>
                     <span style={styles.description}>An app to analyze Facebook Group activity</span>
                     <img
-                      src="/img/facebook-login-button.png"
+                      src={`${prefix}/img/facebook-login-button.png`}
                       alt="facebook-login-button"
                       style={styles.loginBtn}
                       onClick={() => userRepo.login()}
