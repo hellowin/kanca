90c3897ca156cc0e9594643238d332595532e437
Reyhan Sofian <computecoholic@gmail.com>
2017-05-09 16:46:30 +0700

    fix undefined route

--- src/domain/layout/component/NavLink.js
+++ src/domain/layout/component/NavLink.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router'
 
 class NavLink extends Component {
   render() {
+    console.log(this.props);
     return (
       <Link {...this.props} activeClassName="active" />
     );
--- src/infra/service/location.js
+++ src/infra/service/location.js
@@ -2,8 +2,8 @@
 import { browserHistory } from 'react-router';
 import config from 'config';
 
-export const push = (to: string) => browserHistory.push(config.getUrl + to);
-export const getUrl = (to: string) => `${config.getUrl}${to}`;
+export const push = (to: string) => browserHistory.push(config.urlPrefix + to);
+export const getUrl = (to: string) => `${config.urlPrefix}${to}`;
 
 export default {
   push,
