4c1a4d86525311ad3fd8aeef14f54e726f8d3592
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-05-15 09:34:58 +0700

    fix syntax error

--- src/app/App.js
+++ src/app/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
       })
       .then(() => syncToPromise(() => {
         groupRepo.restoreGroup();
-      })
+      }))
       .then(() => {
         loc.push('/metric/summary');
       });
