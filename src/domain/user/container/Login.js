be2fafc6cdef913a743dc0f16b518e11b96b0286
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2017-05-18 12:56:27 +0700

    Fix issues (#158)
    * change login component to dump
    
    * remove login component props
    
    * enhance login page
    
    * fix bugsnag error
    
    * improve error handling on group repository
    
    * fix flow
    
    * fix flow on Alert

--- src/domain/user/container/Login.js
+++ src/domain/user/container/Login.js
@@ -1,62 +1,37 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import MediaQuery from 'react-responsive';
 import userRepo from 'infra/repo/user';
-import loc from 'infra/service/location';
 import styles from './Login.style';
-import githubLogo from 'img/github-logo.png';
 import kancaLogo from 'img/logo.png';
-import facebookLoginImg from 'img/facebook-login-button.png';
 
-const mapStateToProps = state => ({
-  loggedIn: state.user.loggedIn,
-});
-
-class Login extends React.Component {
-
-  componentWillMount() {
-    const { loggedIn } = this.props;
-
-    if (loggedIn) loc.push('/');
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { loggedIn } = nextProps;
-
-    if (loggedIn) loc.push('/');
-  }
-
-  render() {
-    return (
-      <div style={styles.container}>
-          <MediaQuery minDeviceWidth={320} maxWidthDevice={736} minWidth={320} maxWidth={736}>
-            {(matches) => {
-              return (
-                <div>
-                  <div style={styles.header}>
-                    <a href="https://hellowin.github.io/kanca" style={styles.siteName}>Kanca</a>
-                    <a href="https://github.com/hellowin/kanca">
-                      <img src={githubLogo} alt="github-logo" style={styles.logoGithub} />
-                    </a>
-                  </div>
-                  <div style={styles.content}>
-                    <img src={kancaLogo} alt="logo"/>
-                    <span style={styles.description}>An app to analyze Facebook Group activity</span>
-                    <img
-                      src={facebookLoginImg}
-                      alt="facebook-login-button"
-                      style={styles.loginBtn}
-                      onClick={() => userRepo.login()}
-                    />
-                  </div>
+const Login = () => {
+  return (
+    <div style={styles.container}>
+        <MediaQuery minDeviceWidth={320} maxWidthDevice={736} minWidth={320} maxWidth={736}>
+          {(matches) => {
+            return (
+              <div>
+                <a href="https://github.com/hellowin/kanca">
+                  <img
+                    style={{ position: 'absolute', top: 0, right: 0, border: 0 }}
+                    src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+                    alt="Fork me on GitHub"
+                    data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+                  />
+                </a>
+                <div style={styles.content}>
+                  <img src={kancaLogo} alt="logo"/>
+                  <span style={styles.description}>An app to analyze Facebook Group activity</span>
+                  <button type="button" className="btn btn-facebook" onClick={() => userRepo.login()} style={{ cursor: 'pointer' }}>
+                    <span>Login by Facebook</span>
+                  </button>
                 </div>
-              )
-            }}
-          </MediaQuery>
-      </div>
-    );
-  }
-
+              </div>
+            )
+          }}
+        </MediaQuery>
+    </div>
+  );
 }
 
-export default connect(mapStateToProps)(Login);
+export default Login;
---
+++ src/infra/component/Alert.js
@@ -0,0 +1,20 @@
+// @flow
+import React from 'react';
+
+export const AlertLevels = {
+  INFO: 'INFO',
+  SUCCESS: 'SUCCESS',
+  WARNING: 'WARNING',
+  DANGER: 'DANGER',
+}
+
+export type AlertLevel = $Keys<typeof AlertLevels>
+
+export default (props: { level?: AlertLevel }) => {
+  const { children, level } = { level: AlertLevels.INFO.toLowerCase(), ...props };
+  return (
+    <div className={`alert alert-${level.toLowerCase()}`} role="alert">
+      {children}
+    </div>
+  );
+}
